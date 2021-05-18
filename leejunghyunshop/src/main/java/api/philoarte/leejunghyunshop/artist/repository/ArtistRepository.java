f8301a812e1809c92e3851ddd6402989b2fa6f53
LEEJungHyun <akdl911215@naver.com>
2021-05-18 14:35:16 +0900

    테스트 코드 작성

--- leejunghyunshop/src/main/java/api/philoarte/leejunghyunshop/artist/repository/ArtistRepository.java
+++ leejunghyunshop/src/main/java/api/philoarte/leejunghyunshop/artist/repository/ArtistRepository.java
@@ -2,8 +2,11 @@ package api.philoarte.leejunghyunshop.artist.repository;
 
 import api.philoarte.leejunghyunshop.artist.domain.Artist;
 import api.philoarte.leejunghyunshop.artist.domain.ArtistDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -20,9 +23,20 @@ public interface ArtistRepository extends JpaRepository<Artist, Long> {
     @Query("select a from Artist a order by a.artistId desc")
     List<Artist> getAllData();
 
+    @EntityGraph(attributePaths = {"roles"}, type = EntityGraph.EntityGraphType.FETCH)
+    @Query("select a from Artist a group by a  order by a.artistId desc")
+    Page<Artist> getAllDataPaging(Pageable pageable);
+
 
     @Query(value = "select * from artists where username=:username and password=:password", nativeQuery = true)
     Artist signin(@Param("username") String username, @Param("password")String password);
+
+
+    @Modifying
+    @Query("Update FROM Artist a set a.password = :pw where a.artistId = :artistId")
+    int updatePassword(@Param("artistId") Long artistId, @Param("pw") String pw);
+
+
 
 //    @Query("select u from UserVO u where u.username= :username and u.password= :password")
 //    ArtistDto login(@Param("username") String username, @Param("password") String password);
--- leejunghyunshop/src/test/java/api/philoarte/leejunghyunshop/ArtistRepositoryTests.java
+++ leejunghyunshop/src/test/java/api/philoarte/leejunghyunshop/ArtistRepositoryTests.java
@@ -2,17 +2,67 @@ package api.philoarte.leejunghyunshop;
 
 import api.philoarte.leejunghyunshop.artist.domain.Artist;
 import api.philoarte.leejunghyunshop.artist.repository.ArtistRepository;
+import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.annotation.Commit;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @SpringBootTest
+@Log4j2
 public class ArtistRepositoryTests {
 
     @Autowired
     private ArtistRepository repository;
+
+    @Test
+    public void test() {
+        log.info("-----------------------");
+    }
+
+    @Transactional
+    @Commit
+    @Test
+    public void testUpdatePw() {
+
+        repository.updatePassword(8L, "87654321");
+
+
+    }
+
+    @Transactional
+    @Commit
+    @Test
+    public void testUpdate2() {
+
+        Artist artist =  repository.findById(8L).get();
+
+        artist.setPassword("11111");
+        artist.setEmail("aaa@naver.com");
+
+        repository.save(artist);
+
+    }
+
+
+    @Test
+    public void testFindAllOld() {
+
+        Pageable pageable = PageRequest.of(0,10);
+
+        repository.getAllDataPaging(pageable).get().forEach(artist -> {
+            log.info(artist);
+            log.info(artist.getRoles());
+            log.info("--------------------");
+        });
+
+    }
+
 
     @Test
     public void testFindAll() {
